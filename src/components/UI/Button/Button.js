88a9519d0325f959bfece5f0e4825ed3dca00c59
aroran1 <nidhigulati2001@gmail.com>
2021-08-05 00:04:11 +0100

    added media query with styled components

--- src/components/UI/Button/Button.js
+++ src/components/UI/Button/Button.js
@@ -13,6 +13,7 @@
 // these elements are set with unique class names and will never clash with other elements on your app
 import styled from 'styled-components';
 const Button = styled.button`
+  width: 100%;
   font: inherit;
   padding: 0.5rem 1.5rem;
   border: 1px solid #8b005d;
@@ -20,6 +21,10 @@ const Button = styled.button`
   background: #8b005d;
   box-shadow: 0 0 4px rgba(0, 0, 0, 0.26);
   cursor: pointer;
+
+  @media (min-width: 768px) {
+    width: auto;
+  }
 
   &:focus {
     outline: none;

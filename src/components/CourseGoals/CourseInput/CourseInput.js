4ca07a7bfc488d5b66f215ca55bc768463dc7e27
aroran1 <nidhigulati2001@gmail.com>
2021-08-05 00:21:46 +0100

    added module.css

--- src/components/CourseGoals/CourseInput/CourseInput.js
+++ src/components/CourseGoals/CourseInput/CourseInput.js
@@ -1,47 +1,49 @@
 import React, { useState } from 'react';
 
 import Button from '../../UI/Button/Button';
+import styles from './CourseInput.module.css';
 // import './CourseInput.css';
 
 // styled component
-import styled from 'styled-components';
-const FormControl = styled.div`
-  margin: 0.5rem 0;
+// import styled from 'styled-components';
+// const FormControl = styled.div`
+//   margin: 0.5rem 0;
 
-  & label {
-    font-weight: bold;
-    display: block;
-    margin-bottom: 0.5rem;
-    color: ${props => (props.inValid ? 'red' : 'black')};
-  }
+//   & label {
+//     font-weight: bold;
+//     display: block;
+//     margin-bottom: 0.5rem;
+//     color: ${props => (props.inValid ? 'red' : 'black')};
+//   }
 
-  & input {
-    display: block;
-    width: 100%;
-    // border: 1px solid #ccc;
-    border: 1px solid ${props => (props.inValid ? 'red' : '#ccc')};
-    background: ${props => (props.inValid ? '#ffd7d7' : 'transparent')};
-    font: inherit;
-    line-height: 1.5rem;
-    padding: 0 0.25rem;
-  }
+//   & input {
+//     display: block;
+//     width: 100%;
+//     // border: 1px solid #ccc;
+//     border: 1px solid ${props => (props.inValid ? 'red' : '#ccc')};
+//     background: ${props => (props.inValid ? '#ffd7d7' : 'transparent')};
+//     font: inherit;
+//     line-height: 1.5rem;
+//     padding: 0 0.25rem;
+//   }
 
-  & input:focus {
-    outline: none;
-    background: #fad0ec;
-    border-color: #8b005d;
-  }
+//   & input:focus {
+//     outline: none;
+//     background: #fad0ec;
+//     border-color: #8b005d;
+//   }
 
-  // not needed with inValid prop
-  // &.invalid input {
-  //   border-color: red;
-  //   background: #ffd7d7;
-  // }
+//   // not needed with inValid prop
+//   // &.invalid input {
+//   //   border-color: red;
+//   //   background: #ffd7d7;
+//   // }
 
-  // &.invalid label {
-  //   color: red;
-  // }
-`;
+//   // &.invalid label {
+//   //   color: red;
+//   // }
+// `;
+
 
 
 const CourseInput = props => {
@@ -64,23 +66,31 @@ const CourseInput = props => {
 
   return (
     <form onSubmit={formSubmitHandler}>
-      {/* Dynamic class to the form - enable CourseInput.css*/}
+      {/* Dynamic class to the form - uncomment CourseInput.css*/}
       <div className={`form-control ${!isValid ? 'invalid' : ''}`}>
-        {/* <label style={{ color: !isValid ? 'red' : 'black'}}>Course Goal</label>
-        <input style={{ borderColor: !isValid ? 'red' : 'black', backgroundColor: !isValid ? 'salmon' : 'black' }} type="text" onChange={goalInputChangeHandler} /> */}
-        <label>Course Goal</label>
-        <input type="text" onChange={goalInputChangeHandler} />
+        <label style={{ color: !isValid ? 'red' : 'black'}}>Course Goal</label>
+        <input style={{ borderColor: !isValid ? 'red' : '#ccc', backgroundColor: !isValid ? 'salmon' : 'transparent' }} type="text" onChange={goalInputChangeHandler} />
       </div>
+      <Button type="submit">Add Goal</Button> 
 
-      {/* styled component */}
+      {/* styled component - uncomment const FormControl = styled.div` */}
       {/* <FormControl className={ !isValid && 'invalid' }></FormControl> */}
-      <FormControl inValid={!isValid}>
+      {/* <FormControl inValid={!isValid}>
         <label>Course Goal</label>
         <input type="text" onChange={goalInputChangeHandler} />
       </FormControl>
-      <Button type="submit">Add Goal</Button>
+      <Button type="submit">Add Goal</Button> */}
+
+
+      {/* Module CSS */}
+      <div className={`${styles['form-control']} ${!isValid && styles.invalid}`}>
+        <label>Course Goal</label>
+        <input type="text" onChange={goalInputChangeHandler} />
+      </div>
+      <Button type="submit">Add Goal</Button> 
     </form>
   );
 };
+
 
 export default CourseInput;
--- src/components/UI/Button/Button.js
+++ src/components/UI/Button/Button.js
@@ -1,3 +1,4 @@
+// Basic react components with global css
 // import React from 'react';
 // import './Button.css';
 // const Button = props => {
@@ -8,34 +9,50 @@
 //   );
 // };
 
+
 // styles components provides the base elements components and
 // by following code we have just over written the styles of the button provided by styled components
 // these elements are set with unique class names and will never clash with other elements on your app
-import styled from 'styled-components';
-const Button = styled.button`
-  width: 100%;
-  font: inherit;
-  padding: 0.5rem 1.5rem;
-  border: 1px solid #8b005d;
-  color: white;
-  background: #8b005d;
-  box-shadow: 0 0 4px rgba(0, 0, 0, 0.26);
-  cursor: pointer;
+// import styled from 'styled-components';
+// const Button = styled.button`
+//   width: 100%;
+//   font: inherit;
+//   padding: 0.5rem 1.5rem;
+//   border: 1px solid #8b005d;
+//   color: white;
+//   background: #8b005d;
+//   box-shadow: 0 0 4px rgba(0, 0, 0, 0.26);
+//   cursor: pointer;
 
-  @media (min-width: 768px) {
-    width: auto;
-  }
+//   @media (min-width: 768px) {
+//     width: auto;
+//   }
 
-  &:focus {
-    outline: none;
-  }
+//   &:focus {
+//     outline: none;
+//   }
 
-  &:hover,
-  &:active {
-    background: #ac0e77;
-    border-color: #ac0e77;
-    box-shadow: 0 0 8px rgba(0, 0, 0, 0.26);
-  }
-`;
+//   &:hover,
+//   &:active {
+//     background: #ac0e77;
+//     border-color: #ac0e77;
+//     box-shadow: 0 0 8px rgba(0, 0, 0, 0.26);
+//   }
+// `;
+
+// export default Button;
+
+
+// Modules CSS - react by default comes with this
+// this maintains the seperation between css and javascript
+import React from 'react';
+import styles from './Button.module.css';
+const Button = props => {
+  return (
+    <button type={props.type} className={styles.button} onClick={props.onClick}>
+      {props.children}
+    </button>
+  );
+};
 
 export default Button;
